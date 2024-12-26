ce82f184c318d95da4991254929026e76541ef9f
NicolasTecser11 <desarrollador11@tecsersas.com>
2024-12-26 09:12:46 -0500

    The standardized response function is refactored

--- src/main/java/com/cgr/base/presentation/controller/AbstractController.java
+++ src/main/java/com/cgr/base/presentation/controller/AbstractController.java
@@ -10,15 +10,12 @@ import java.util.function.Supplier;
 
 public abstract class AbstractController {
 
-    protected ResponseEntity<?> requestResponse(BindingResult result, Supplier<?> action, String message) {
+    protected ResponseEntity<?> requestResponse(BindingResult result, Supplier<?> action, String message,
+            HttpStatus status, boolean successful) {
         if (result.hasFieldErrors()) {
             return buildValidationErrorResponse(result);
         }
-        return buildResponse(action.get(), message, HttpStatus.OK, true);
-    }
-
-    protected ResponseEntity<?> requestResponse(Object data, String message) {
-        return buildResponse(data, message, HttpStatus.OK, true);
+        return buildResponse(action.get(), message, status, successful);
     }
 
     protected ResponseEntity<?> requestResponse(Object data, String message, HttpStatus status, boolean successful) {
@@ -26,8 +23,12 @@ public abstract class AbstractController {
     }
 
     private ResponseEntity<?> buildResponse(Object data, String message, HttpStatus status, boolean successful) {
-        Map<String, Object> responseBody = createResponseBody(data, message, status, successful);
-        return ResponseEntity.status(status).body(responseBody);
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", data);
+        response.put("status", status.value());
+        response.put("successful", successful);
+        response.put(successful ? "message" : "error", message);
+        return ResponseEntity.status(status).body(response);
     }
 
     private ResponseEntity<?> buildValidationErrorResponse(BindingResult result) {
@@ -35,14 +36,5 @@ public abstract class AbstractController {
         result.getFieldErrors().forEach(error -> errors.put(error.getField(),
                 String.format("El campo %s %s", error.getField(), error.getDefaultMessage())));
         return buildResponse(errors, "Error de validación de JSON", HttpStatus.BAD_REQUEST, false);
-    }
-
-    private Map<String, Object> createResponseBody(Object data, String message, HttpStatus status, boolean successful) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("data", data);
-        response.put("status", status.value());
-        response.put("successful", successful);
-        response.put(successful ? "message" : "error", message);
-        return response;
     }
 }
