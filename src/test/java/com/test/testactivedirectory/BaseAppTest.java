88df06a3e8f5729f181ef4627de6d53839b4876f
DevopsTecser <desarrollador5@tecsersas.com>
2024-12-27 14:04:58 -0500

    Base Test Class Fix

--- src/test/java/com/test/testactivedirectory/BaseAppTest.java
+++ src/test/java/com/test/testactivedirectory/BaseAppTest.java
@@ -3,7 +3,9 @@ package com.test.testactivedirectory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.cgr.base.BaseApp;
+
+@SpringBootTest(classes = BaseApp.class)
 class BaseAppTest {
 
 	@Test
