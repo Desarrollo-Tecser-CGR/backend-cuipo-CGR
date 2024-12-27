c2b3f6727cbe9a84f806ed41eb91b1f5c7b3e15b
A2GROUP\Miguel.fayad <desarrollador5@tecsersas.com>
2024-12-27 17:33:25 -0500

    Database creation

---
+++ src/main/java/com/test/testactivedirectory/infrastructure/persistence/entity/Expense.java
@@ -0,0 +1,41 @@
+package com.test.testactivedirectory.infrastructure.persistence.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Expenses")
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int expenseId;
+
+    @ManyToOne
+    @JoinColumn(name = "programContractId")
+    private Program program;
+
+    @Column(nullable = false, length = 30)
+    private String category;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date expenseDate;
+
+    // Getters and Setters
+}
---
+++ src/main/java/com/test/testactivedirectory/infrastructure/persistence/entity/Objective.java
@@ -0,0 +1,34 @@
+package com.test.testactivedirectory.infrastructure.persistence.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Objectives")
+public class Objective {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int objectiveId;
+
+    @Column(nullable = false, length = 65533)
+    private String description;
+
+    @Column(nullable = false, length = 65533)
+    private String complianceIndicators;
+
+    @Temporal(TemporalType.DATE)
+    private Date targetDate;
+
+    // Getters and Setters
+}
