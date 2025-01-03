9fb6df17ad072513a86f3d6fa743839ee046db2f
Giseth-Murcia <desarrollador15@tecsersas.com>
2025-01-03 15:36:54 -0500

    AddTable_GeneralRules_Rule4

--- src/main/java/com/cgr/base/application/GeneralRules/GeneralRulesManager.java
+++ src/main/java/com/cgr/base/application/GeneralRules/GeneralRulesManager.java
@@ -128,22 +128,56 @@ public class GeneralRulesManager {
             ).findFirst();
     
             if (matchingEntry.isPresent()) {
+
                 DataProgIngresos matchedData = matchingEntry.get();
+
+                // Regla 1: Presupuesto Definitivo
                 Double presupuestoDefinitivoValue = matchedData.getPresupuestoDefinitivo();
                 String resultGeneralRule1 = evaluateGeneralRule1(presupuestoDefinitivoValue);
                 generalRule.setGeneralRule1(resultGeneralRule1);
+
+                // Regla 4: Comparativo de Campos
+                Double presupuestoInicialValue = matchedData.getPresupuestoInicial();
+                String resultGeneralRule4 = evaluateGeneralRule4(presupuestoDefinitivoValue, presupuestoInicialValue);
+                generalRule.setGeneralRule4(resultGeneralRule4);
+
             } else {
                 generalRule.setGeneralRule1("NO DATA");
+                generalRule.setGeneralRule4("NO DATA");
             }
+
+            // Regla2: Entidad en Liquidacion.
+            String accountNameValue = generalRule.getAccountName();
+            String resultGeneralRule2 = evaluateGeneralRule2(accountNameValue);
+            generalRule.setGeneralRule2(resultGeneralRule2);
+
+            // Guardar Cambios
             generalRulesRepository.save(generalRule);
         });
     }
 
+    // Regla1: Validacion presupuesto definitivo.
     private String evaluateGeneralRule1(Double value) {
         if (value == null || value.isNaN()) {
             value = 0.0;
         }
         return value > 100000000 ? "CUMPLE" : "NO CUMPLE";
+    }
+
+    // Regla2: Entidad en Liquidacion.
+    public String evaluateGeneralRule2(String value) {
+        return value != null && value.toLowerCase().contains("liquidacion") ? "NO CUMPLE" : "CUMPLE";
+    }
+
+    // Regla4: Comparativo de Campos.
+    public String evaluateGeneralRule4(Double value1, Double value2) {
+        if (value1 == null || value1.isNaN()) {
+            value1 = 0.0;
+        }
+        if (value2 == null || value2.isNaN()) {
+            value2 = 0.0;
+        }
+        return (value1 == 0.0 && value2 == 0.0) ? "NO CUMPLE" : "CUMPLE";
     }
 
     @Transactional
