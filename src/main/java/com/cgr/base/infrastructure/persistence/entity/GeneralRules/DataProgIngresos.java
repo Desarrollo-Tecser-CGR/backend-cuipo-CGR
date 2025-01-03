e791a519c53f2921d27e90eb93fe46708ccec3bf
Giseth-Murcia <desarrollador15@tecsersas.com>
2025-01-03 15:22:01 -0500

    TotalData_Rule1

---
+++ src/main/java/com/cgr/base/infrastructure/persistence/entity/GeneralRules/DataProgIngresos.java
@@ -0,0 +1,34 @@
+package com.cgr.base.infrastructure.persistence.entity.GeneralRules;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "TMP_VW_OPENDATA_A_PROGRAMACION_INGRESOS", schema = "CUIPO")
+@NoArgsConstructor
+public class DataProgIngresos {
+
+   @Column(name = "PERIODO")
+   private String periodo;
+
+   @Column(name = "AMBITO_NOMBRE")
+   private String nombreAmbito;
+
+   @Column(name = "NOMBRE_ENTIDAD")
+   private String nombreEntidad;
+
+   @Id
+   @Column(name = "NOMBRE_CUENTA")
+   private String nombreCuenta;
+   
+   @Column(name = "PRESUPUESTO_DEFINITIVO")
+   private Double presupuestoDefinitivo;
+
+   @Column(name = "PRESUPUESTO_INICIAL")
+   private Double presupuestoInicial;
+}
