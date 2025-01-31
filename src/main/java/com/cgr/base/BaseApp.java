4f7e0f46e6e53e9b5804e1c8a3a65b9ac809e545
Giseth-Murcia <desarrollador15@tecsersas.com>
2025-01-31 11:18:04 -0500

    EnviromentVariables

--- src/main/java/com/cgr/base/BaseApp.java
+++ src/main/java/com/cgr/base/BaseApp.java
@@ -3,11 +3,25 @@ package com.cgr.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BaseApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaseApp.class, args);
-	}
+    public static void main(String[] args) {
 
+        // Cargar variables de entorno desde el archivo .env
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+
+        // Establecer las variables de entorno como propiedades del sistema
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+        // Iniciar la aplicación Spring Boot
+        SpringApplication.run(BaseApp.class, args);
+    }
 }
