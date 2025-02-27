1933d30f5e3df3c2a597f9510eb792846db7ad68
Jhonn Baracaldo <jhon.baracaldo@tecsersas.com>
2025-02-27 14:40:38 -0500

    Alter deparments

--- src/main/java/com/cgr/base/application/maps/entity/dtoMaps/deparment/DepartmentsDto.java
+++ src/main/java/com/cgr/base/application/maps/entity/dtoMaps/deparment/DepartmentsDto.java
@@ -1,4 +1,16 @@
 package com.cgr.base.application.maps.entity.dtoMaps.deparment;
 
+import com.cgr.base.application.maps.entity.dtoMaps.municipality.MunicipalityDto;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class DepartmentsDto {
+
+    private Long department_id;
+    private String dpto_cnmbr;
+    private String dpto_ccdgo;
+    private String image;
+    private List<MunicipalityDto> municipalities;
 }
