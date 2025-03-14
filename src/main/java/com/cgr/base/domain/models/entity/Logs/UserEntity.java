fb3cbb94b15367a5f6024ad45ed03b2167dbe8e0
jhonbaracaldo <desarrollador20@tecsersas.com>
2025-03-14 10:17:23 -0500

    Refactored AuthService to ensure failed login attempts are properly logged and verified before account locking

--- src/main/java/com/cgr/base/domain/models/entity/Logs/UserEntity.java
+++ src/main/java/com/cgr/base/domain/models/entity/Logs/UserEntity.java
@@ -1,5 +1,6 @@
 package com.cgr.base.domain.models.entity.Logs;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -18,6 +19,7 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -52,6 +54,15 @@ public class UserEntity {
 
     @Column(name = "user_type")
     private String userType;
+
+    //jhon
+    @Column(name = "failed_attempts")
+    private Integer failedAttempts = 0;
+
+    @Column(name = "lock_time")
+    private LocalDateTime lockTime;
+
+
 
     @OneToMany(mappedBy = "user")
     private List<LogEntity> logs = new ArrayList<>();
