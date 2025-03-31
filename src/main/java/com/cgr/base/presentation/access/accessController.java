e4c06e026d3ec8994b7771670649504ef20a18d6
A2GROUP\william.florian <desarrollador25@tecsersas.com>
2025-03-31 15:11:40 -0500

    Update JwtAuthFilter to include new module endpoints and remove unused PreAuthorize annotation from accessController

--- src/main/java/com/cgr/base/presentation/access/accessController.java
+++ src/main/java/com/cgr/base/presentation/access/accessController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +24,6 @@ public class accessController extends AbstractController {
     @Autowired
     accessManagement Access;
 
-    @PreAuthorize("hasAuthority('MENU_1')")
     @GetMapping("/module/list")
     public ResponseEntity<?> getAvailableMenus() {
         List<Map<String, Object>> menus = Access.getAvailableMenus();
