394724b32726c43cbcfa91d89ff6a5908b4cf753
jhonbaracaldo <desarrollador20@tecsersas.com>
2025-04-02 18:07:11 -0500

    adding changue quantity mounth

--- src/main/java/com/cgr/base/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/com/cgr/base/infrastructure/security/config/SecurityConfig.java
@@ -55,6 +55,9 @@ public class SecurityConfig {
                                         auth.requestMatchers("/api/v1/logsExit/**").hasAnyAuthority("auditor", "administrador", "analista");
                                         auth.requestMatchers("/api/v1/dinamic-menus/**").hasAnyAuthority("auditor", "administrador", "analista");
                                     auth.requestMatchers("/api/v1/AuditEntry/**").hasAnyAuthority("auditor", "administrador", "analista");
+                                    auth.requestMatchers("/api/v1/logs/**").hasAnyAuthority("auditor", "administrador", "analista");
+
+
 
                                 });
 
